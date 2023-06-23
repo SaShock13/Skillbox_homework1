9dd695c4d1f8981b57f72ee573e0c837a5928599
BEREZOV ALEKSANDR <137479900+SaShock13@users.noreply.github.com>
2023-06-23 11:12:33 +0400

    Add files via upload

---
+++ SkillBoxHomework1/SkillBoxHomework1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillBoxHomework1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Привет ");
+            Write("Мир ");
+            WriteLine("!!!!!!!!!!!!!!!!");
+            ReadKey();
+        }
+    }
+}
